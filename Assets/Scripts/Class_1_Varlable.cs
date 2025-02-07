616680cab85ccdc1e42bd72e8f4bb46fd0b43d01
lodiya000 <94903931+lodiya000@users.noreply.github.com>
2025-02-07 18:50:34 +0800

    Update  認識資料類型

--- Assets/Scripts/Class_1_Varlable.cs
+++ Assets/Scripts/Class_1_Varlable.cs
@@ -1,6 +1,20 @@
-using UnityEngine;
+﻿using UnityEngine;
+//2025/2/7
 
-public class Class_1_Varlable
+/*第一行
+第二行
+ 
+ */
+
+//修飾詞 類別關鍵詞(腳本) 類別名稱(腳本名稱)
+//在腳本後添加 :MonoBehaviour 讓此腳本可以掛在Unity的物件上上
+public class Class_1_Varlable : MonoBehaviour
 {
-    
+    //公開:允許其他類別存取 public
+    //私人:不允許其他類別存取 private(預設)
+    public int cost = 999;
+    private float jumphight = 77.5f;
+    public string weapon = "三項之力";
+    private bool dragensoul = true;
+
 }
---
+++ Assets/Scripts/Class_2_DateType.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 認識資料類型
+/// </summary>
+public class Class_2_DateType: MonoBehaviour 
+{
+    public float a = 7.7f;
+    public double b = 0.12345;
+    public decimal c = 1.23456789m;
+}
